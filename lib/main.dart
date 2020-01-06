6b4b93d75939299764f8922e25e2318374c4c52c
Jirawat <jirawatfreedom@gmail.com>
2020-01-06 15:04:22 +0700

    spit quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,70 @@
 import 'package:flutter/material.dart';
-import './question.dart';
+import './quiz.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
 }
+
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color ? ',
-      'What\'s your favorite animal ? '
+    final _questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': [
+          'Black',
+          'Red',
+          'Green',
+          'White',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': [
+          'Rabbit',
+          'Snake',
+          'Elephant',
+          'Lion',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite instructor?',
+        'answers': [
+          'Home',
+          'Tower',
+          'Elephant',
+          'Lion',
+        ],
+      },
     ];
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: Text('My First App'),
-            ),
-            body: Column(
-                children: [
-                  Question(
-                      questions[_questionIndex],
-                  ),
-                  RaisedButton(
-                      child: Text('Answer 1'),
-                      onPressed: answerQuestion,
-                  ),
-                  RaisedButton(
-                      child: Text('Answer 2'),
-                      onPressed: () => print('Answer 2 chosen!'),
-                  ),
-                  RaisedButton(
-                      child: Text('Answer 3'),
-                      onPressed: () {
-                        print('Answer 3 chosen!');
-                      },
-                  ),
-                ],
-            ),
-            ),
-            );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
+      ),
+    );
   }
 }
